567c56fad801bbba2591371e674218fe24e2e04b
64001449 <101417234+64001449@users.noreply.github.com>
2022-06-04 11:02:34 -0500

    Add files via upload

---
+++ FinalProjectObject.java
@@ -0,0 +1,27 @@
+package Classwork;
+
+public class FinalProjectObject {
+	private String eyeColor;
+	private String hair;
+	private String height;
+	private String name;
+	
+	public FinalProjectObject(String e, String h, String he, String n) {
+		eyeColor = e;
+		hair = h;
+		height = he;
+		name = n;
+	}
+	public String getEye() {
+		return eyeColor;
+	}
+	public String getHair() {
+		return hair;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public String getName() {
+		return name;
+	}
+}
